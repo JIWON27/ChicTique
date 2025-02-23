e7ce03b96c573ae1c3a0950fe5c2c0d0cb96b781
minjiwon <mjo4260@naver.com>
2025-02-23 10:29:19 +0900

    chore: 최초 커밋

---
+++ src/main/java/com/ecommerce/chictique/ChicTiqueApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.chictique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChicTiqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChicTiqueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecommerce/chictique/ChicTiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.chictique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChicTiqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
